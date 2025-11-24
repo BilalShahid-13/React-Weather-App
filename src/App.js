5f71aace2e67ffe9c50222de032b9f124f5af3c7
BilalShahid-13 <bcsm-f21-249@superior.edu.pk>
2025-11-25 03:05:49 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { gsap } from "gsap";
 import { Component } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -11,19 +11,6 @@ import Search from "./components/Search";
 import Weather from "./components/Weather";
 import Week from "./components/Week";
 import SplashScreen from "./Loaders/SplashScreen";
-import React, { Component } from "react";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Search from "./components/Search";
-import Dashboard from "./components/Dashboard";
-import Home from "./components/Home";
-import Weather from "./components/Weather";
-import Week from "./components/Week";
-import Hourly from "./components/Hourly";
-import Footer from "./components/Footer";
-import Info from "./components/Info";
-import SplashScreen from "./Loaders/SplashScreen";
-import { gsap } from "gsap";
 export class App extends Component {
   constructor(props) {
     super(props);
@@ -60,7 +47,7 @@ export class App extends Component {
           <>
             {/* <SplashScreen /> */}
 
-              <Navbar />
+            <Navbar />
             <main className="space-y-2">
               <Search />
               {/* <Home /> */}
@@ -98,50 +85,6 @@ export class App extends Component {
         )}
       </>
     );
-    return () => {
-      <>
-        <Router>
-          {this.state.splash ? (
-            <SplashScreen />
-          ) : (
-            <>
-              {/* <SplashScreen /> */}
-
-              <main className="space-y-2">
-                <Navbar />
-                <Search />
-                {/* <Home /> */}
-                <Routes>
-                  <Route path="/Dashboard" element={<Dashboard />} />
-                  <Route exact element={<Home />} path="/" />
-                  <Route
-                    path="/today"
-                    element={
-                      <Weather
-                        input={this.state.input}
-                        isFahrenheit={this.state.isFarenheit}
-                      />
-                    }
-                  />
-                  <Route
-                    path="/week"
-                    element={<Week input={this.state.input} />}
-                  />
-                  <Route
-                    path="/hourly"
-                    element={<Hourly input={this.state.input} />}
-                  />
-                  {/* <Route path="/Setting" element={<Settings />} /> */}
-                  <Route path="/Info" element={<Info />} />
-                  {/* <Route path="/Navbar" element={<Navbar />} /> */}
-                </Routes>
-                <Footer />
-              </main>
-            </>
-          )}
-        </Router>
-      </>;
-    };
   }
 }
 
